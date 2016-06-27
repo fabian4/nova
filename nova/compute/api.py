38c4d0b41a195ed07e13196e52db16f9cb6ef0ea
fabian4 <cybing4@gmail.com>
2016-06-27 18:35:47 +0800

    Fix soft_delete,abnormal quota usage after restore by admin Use upstream commit: c474b44bdcdda223b0ec90b7fa1673501cf690e7 Redmine #7099 http://192.168.15.2/issues/7099
    Signed-off-by: fabian <cybing4@gmail.com>

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -350,7 +350,7 @@ class API(base.Base):
                 raise exception.OnsetFileContentLimitExceeded()
 
     def _check_num_instances_quota(self, context, instance_type, min_count,
-                                   max_count):
+                                   max_count, project_id=None, user_id=None):
         """Enforce quota limits on number of instances created."""
 
         # Determine requested cores and ram
@@ -362,7 +362,8 @@ class API(base.Base):
         try:
             quotas = objects.Quotas(context)
             quotas.reserve(context, instances=max_count,
-                           cores=req_cores, ram=req_ram)
+                           cores=req_cores, ram=req_ram,
+                           project_id=project_id, user_id=user_id)
         except exception.OverQuota as exc:
             # OK, we exceeded quota; let's figure out why...
             quotas = exc.kwargs['quotas']
@@ -1799,8 +1800,9 @@ class API(base.Base):
         """Restore a previously deleted (but not reclaimed) instance."""
         # Reserve quotas
         flavor = instance.get_flavor()
+        project_id, user_id = quotas_obj.ids_from_instance(context, instance)
         num_instances, quotas = self._check_num_instances_quota(
-                context, flavor, 1, 1)
+            context, flavor, 1, 1, project_id=project_id, user_id=user_id)
 
         self._record_action_start(context, instance, instance_actions.RESTORE)
 
